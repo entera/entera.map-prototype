ff40d9216f14bbc8310a4b2fb3d5470b58cf70ad
Benjamin Gudehus <hastebrot@gmail.com>
2016-08-31 05:52:56 +0200

    (refactor) Draw the plot grid in a more functional way.

--- projects/entera.plot/README.md
+++ projects/entera.plot/README.md
@@ -1,3 +1,13 @@
 ## README
 
+### Motivation
 
+
+
+### Features
+
+- [ ] Automatic translate/scale of view.
+- [ ] Draw geometries (points, segments, polylines).
+- [ ] Color schemes (Brewer's Set1, D3's Category20c).
+- [ ] Grid marker with points (JTS Test Runner) or lines (Seaborn/Matplotlib).
+- [ ] Grid labels.
--- projects/entera.plot/src/test/java/plot.kt
+++ projects/entera.plot/src/test/java/plot.kt
@@ -1,7 +1,10 @@
+import javafx.event.EventHandler
 import javafx.geometry.BoundingBox
 import javafx.geometry.Bounds
+import javafx.geometry.Point2D
 import javafx.scene.Scene
-import javafx.scene.layout.AnchorPane
+import javafx.scene.input.KeyCode
+import javafx.scene.layout.Pane
 import javafx.scene.paint.Color
 import javafx.scene.shape.Line
 import javafx.scene.shape.Rectangle
@@ -12,25 +15,45 @@ import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>) {
     setupStage { stage ->
-        val gridColor = Color.gray(0.95).interpolate(Color.BLUE, 0.05)
-
-        val canvas = AnchorPane()
+        // canvas.
+        val canvas = Pane()
         canvas.style = "-fx-background-color: white;"
 
-        val region = bounds(100.0, 100.0, 300.0, 300.0)
+        // plot.
+        val plot = plot()
+        canvas.children += plot
 
-        canvas.children += Rectangle(region.minX, region.minY, region.width, region.height)
-            .apply { fill = gridColor }
+        // grid.
+        val gridRegion = bounds(100.0, 100.0, 300.0, 300.0)
+        val gridColor = Color.gray(0.95).interpolate(Color.BLUE, 0.05)
 
-        canvas.children += Line(250.0 - 0.5, 0.0, 250.0 - 0.5, 500.0)
-            .apply { stroke = Color.WHITE; opacity = 1.0 }
-        canvas.children += Line(0.0, 250.0 - 0.5, 500.0, 250.0 - 0.5)
-            .apply { stroke = Color.WHITE }
+        listOf(gridRegion)
+            .map { Rectangle(it.minX, it.minY, it.width, it.height) }
+            .map { it.fill = gridColor; it }
+            .toCollection(plot.children)
+        listOf(0.25, 0.5, 0.75)
+            .map { gridRegion.interpolate(point(it, 1.0)) }
+            .map { Line(it.x - 0.5, 0.0, it.x - 0.5, it.y ) }
+            .map { it.stroke = Color.WHITE; it }
+            .toCollection(plot.children)
+        listOf(0.25, 0.5, 0.75)
+            .map { gridRegion.interpolate(point(1.0, it)) }
+            .map { Line(0.0, it.y - 0.5, it.x, it.y - 0.5 ) }
+            .map { it.stroke = Color.WHITE; it }
+            .toCollection(plot.children)
 
+        // scene.
         stage.scene = Scene(canvas, 500.0, 500.0)
+        stage.scene.onKeyPressed = EventHandler {
+            when(it.code) { KeyCode.ESCAPE, KeyCode.Q -> stage.close() }
+        }
     }
 }
 
+fun plot() = Plot()
+class Plot : Pane()
+
+private fun point(x: Double, y: Double) = Point2D(x, y)
 private fun bounds(minX: Double, minY: Double,
                    width: Double, height: Double): Bounds =
     BoundingBox(minX, minY, width, height)
@@ -42,3 +65,11 @@ private fun setupStage(action: (stage: Stage) -> Unit) {
     FxToolkit.showStage()
     WaitForAsyncUtils.waitFor(1, TimeUnit.HOURS) { !stage.isShowing }
 }
+
+private fun ClosedRange<Double>.interpolate(factor: Double): Double =
+    start + ((endInclusive - start) * factor)
+
+private fun Bounds.interpolate(factor: Point2D): Point2D = Point2D(
+    (minX..maxX).interpolate(factor.x),
+    (minY..maxY).interpolate(factor.y)
+)
